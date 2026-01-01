94153e2e5e85102b258b939b5d15937021352770
mallouk-bike <spotify.test0707@gmail.com>
2026-01-01 14:53:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mallouk-bike-v1";
+const CACHE_NAME = "mallouk-bike-v2";
 const URLS_TO_CACHE = [
   "/Mallouk-Bike/"
 ];
@@ -6,6 +6,20 @@ const URLS_TO_CACHE = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
